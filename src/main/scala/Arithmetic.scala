eecfa98dbec400808d075f5aec758d3a7c639903
Torstein Vik <torsteinv64@gmail.com>
2018-01-26 19:03:25 +0100

    Added Nat, Integer, Foating, and Ratio

--- src/main/scala/Arithmetic.scala
+++ src/main/scala/Arithmetic.scala
@@ -5,4 +5,8 @@ object Arithmetic {
     sealed abstract class ComplexNumber
     sealed abstract class Real extends ComplexNumber
 
+    sealed case class Nat      (x : BigInt) extends Real {require(x > 0)}
+    sealed case class Integer  (x : BigInt) extends Real
+    sealed case class Floating (x : Float)  extends Real
+    sealed case class Ratio    (num : Integer, den : Integer) extends Real {require(realToFloat(den) != 0)}
 }

d79001d9b66eed455f04bafe32845b0fdd8844cf
Torstein Vik <torsteinv64@gmail.com>
2018-01-26 18:18:42 +0100

    Added package file

---
+++ src/main/scala/package.scala
@@ -0,0 +1,6 @@
+package io.github.torsteinvik.zetatypes
+
+/** This pacakge provides tools for working with a database of multiplicative functions, in the spec. of https://github.com/torstein-vik/multfunc-db 
+ *  @author Torstein Vik
+ */
+package object db {}

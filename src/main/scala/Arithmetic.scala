9db0570a1443d533b107324d2ee09cc08abbb1f2
Torstein Vik <torsteinv64@gmail.com>
2018-01-26 19:03:49 +0100

    Added implicit conversion Real => Float

--- src/main/scala/Arithmetic.scala
+++ src/main/scala/Arithmetic.scala
@@ -2,6 +2,8 @@ package io.github.torsteinvik.zetatypes.db
 
 object Arithmetic {
 
+    import scala.language.implicitConversions
+
     sealed abstract class ComplexNumber
     sealed abstract class Real extends ComplexNumber
 
@@ -12,5 +14,12 @@ object Arithmetic {
 
     sealed case class CartesianComplex (re : Real, im : Real) extends ComplexNumber
     sealed case class PolarComplex (abs : Real, unitarg : Real) extends ComplexNumber {require(abs >= 0 && unitarg >= 0 && unitarg < 1)}
+
+    implicit def realToFloat(x : Real) : Float = x match {
+        case Nat(x) => x.floatValue
+        case Integer(x) => x.floatValue
+        case Floating(x) => x
+        case Ratio(Integer(x), Integer(y)) => x.floatValue / y.floatValue
+    }
 
 }

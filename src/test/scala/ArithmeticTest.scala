03600f4ce3ad1f8d6c7048d2fa2d6017648c2f4f
Torstein Vik <torsteinv64@gmail.com>
2018-01-26 19:04:26 +0100

    Added Natural number reqs.

--- src/test/scala/ArithmeticTest.scala
+++ src/test/scala/ArithmeticTest.scala
@@ -6,5 +6,11 @@ import io.github.torsteinvik.zetatypes.db.Arithmetic._
 
 class ArithmeticTest extends FunSuite {
     
+    test ("Natural number requirements") {
+        assertThrows[IllegalArgumentException]{ Nat(-10) }
+        assertThrows[IllegalArgumentException]{ Nat(0) }
+        ;{ Nat(1) }
+        ;{ Nat(10) }
+    }
     
 }

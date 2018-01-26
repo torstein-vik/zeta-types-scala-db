8d051e1be065af02e18058331878fe59be27b6f3
Torstein Vik <torsteinv64@gmail.com>
2018-01-26 19:04:12 +0100

    Added class for Arithmetic types test

---
+++ src/test/scala/ArithmeticTest.scala
@@ -0,0 +1,10 @@
+package io.github.torsteinvik.zetatypes.test.db
+
+import org.scalatest.FunSuite
+
+import io.github.torsteinvik.zetatypes.db.Arithmetic._
+
+class ArithmeticTest extends FunSuite {
+    
+    
+}

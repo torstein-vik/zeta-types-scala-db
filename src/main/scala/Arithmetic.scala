c14b070a8cf4b8e737dde368a37426db76d93a00
Torstein Vik <torsteinv64@gmail.com>
2018-01-26 19:03:34 +0100

    Added complex numbers

--- src/main/scala/Arithmetic.scala
+++ src/main/scala/Arithmetic.scala
@@ -9,4 +9,8 @@ object Arithmetic {
     sealed case class Integer  (x : BigInt) extends Real
     sealed case class Floating (x : Float)  extends Real
     sealed case class Ratio    (num : Integer, den : Integer) extends Real {require(realToFloat(den) != 0)}
+
+    sealed case class CartesianComplex (re : Real, im : Real) extends ComplexNumber
+    sealed case class PolarComplex (abs : Real, unitarg : Real) extends ComplexNumber {require(abs >= 0 && unitarg >= 0 && unitarg < 1)}
+
 }

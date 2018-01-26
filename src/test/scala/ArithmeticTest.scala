c843f4cb5b684fda9e5da2fde5b1b0626bf9070e
Torstein Vik <torsteinv64@gmail.com>
2018-01-26 19:04:50 +0100

    Added complex polar req. test

--- src/test/scala/ArithmeticTest.scala
+++ src/test/scala/ArithmeticTest.scala
@@ -20,4 +20,23 @@ class ArithmeticTest extends FunSuite {
         ;{ Ratio(Integer(10), Integer(-15)) }
     }
     
+    test ("Complex Polar requirements") {
+        assertThrows[IllegalArgumentException]{ PolarComplex(Integer(-10), Integer(0)) }
+        assertThrows[IllegalArgumentException]{ PolarComplex(Integer(-10), Floating(0.5F)) }
+        assertThrows[IllegalArgumentException]{ PolarComplex(Floating(-0.00001F), Floating(0.5F)) }
+        ;{ PolarComplex(Integer(0), Integer(0)) }
+        ;{ PolarComplex(Integer(10), Integer(0)) }
+        ;{ PolarComplex(Integer(10), Floating(0.5F)) }
+        
+        assertThrows[IllegalArgumentException]{ PolarComplex(Integer(10), Integer(-1)) }
+        assertThrows[IllegalArgumentException]{ PolarComplex(Integer(10), Floating(-5F)) }
+        assertThrows[IllegalArgumentException]{ PolarComplex(Integer(10), Floating(-0.00001F)) }
+        assertThrows[IllegalArgumentException]{ PolarComplex(Integer(10), Floating(1.00001F)) }
+        assertThrows[IllegalArgumentException]{ PolarComplex(Integer(10), Floating(5F)) }
+        assertThrows[IllegalArgumentException]{ PolarComplex(Integer(10), Integer(2)) }
+        ;{ PolarComplex(Integer(10), Integer(0)) }
+        ;{ PolarComplex(Integer(10), Ratio(Integer(2), Integer(4))) }
+        ;{ PolarComplex(Integer(10), Floating(0.5F)) }
+    }
+    
 }

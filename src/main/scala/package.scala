0bbc0b2bea4067605046c568242d8affa21995dc
Torstein Vik <torsteinv64@gmail.com>
2018-02-08 21:48:41 +0100

    Fixed URL type

--- src/main/scala/package.scala
+++ src/main/scala/package.scala
@@ -1,6 +1,6 @@
 package io.github.torsteinvik.zetatypes
 
-/** This pacakge provides tools for working with a database of multiplicative functions, in the spec. of https://github.com/torstein-vik/multfunc-db 
+/** This pacakge provides tools for working with a database of multiplicative functions, in the spec. of https://github.com/torstein-vik/multfuncs-db 
  *  @author Torstein Vik
  */
 package object db {}

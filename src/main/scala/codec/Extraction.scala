d35d37119b7fb707fe25a24d0e72d47876c4e049
Torstein Vik <torsteinv64@gmail.com>
2018-04-18 00:07:11 +0200

    Added codecs for bigint, double, and string

--- src/main/scala/codec/Extraction.scala
+++ src/main/scala/codec/Extraction.scala
@@ -8,6 +8,31 @@ private[codec] object Extraction {
     def apply[T](implicit tt : TypeTag[T]) : Codec[_] = {
     }
     
+    private object BigIntCodec extends Codec[BigInt]{
+        // TODO: Note not up to spec, weird [base, 32, [...]] thing not accounted for, seems uneeded
+        def encode (x : BigInt) : JValue = JInt(x)
+        def decode (x : JValue) : BigInt = x match {
+            case JInt(num) => num
+            case _ => throw new Exception("Found " + x + " expected BigInt (JInt)")
+        }
+    }
+    
+    private object DoubleCodec extends Codec[Double]{
+        // TODO: Note not up to spec, spec uses floating numbers
+        def encode (x : Double) : JValue = JDouble(x)
+        def decode (x : JValue) : Double = x match {
+            case JDouble(num) => num
+            case _ => throw new Exception("Found " + x + " expected Double (JDouble)")
+        }
+    }
+    
+    private object StringCodec extends Codec[String]{
+        def encode (x : String) : JValue = JString(x)
+        def decode (x : JValue) : String = x match {
+            case JString(str) => str
+            case _ => throw new Exception("Found " + x + " expected String (JString)")
+        }
+    }
 }
 
 /*

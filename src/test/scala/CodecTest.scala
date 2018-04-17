c995d08820479bfc3a38570765ab1373bbfe0ce9
Torstein Vik <torsteinv64@gmail.com>
2018-04-17 21:17:32 +0200

    Added Codec-creation test

--- src/test/scala/CodecTest.scala
+++ src/test/scala/CodecTest.scala
@@ -2,8 +2,16 @@ package io.github.torsteinvik.zetatypes.test.db
 
 import org.scalatest.FunSuite
 
+import io.github.torsteinvik.zetatypes.db._
+import io.github.torsteinvik.zetatypes.db.Datatypes._
+import io.github.torsteinvik.zetatypes.db.codec._
 
 class CodecTest extends FunSuite {
     
+    test("Define codec for MultiplicativeFunction") { Codec[MultiplicativeFunction] }
+    
+    test("Encode MultiplicativeFunction"){
+        val codec = Codec[MultiplicativeFunction]
+    }
     
 }

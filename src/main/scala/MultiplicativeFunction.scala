7e9552c6b48b39e8f23caa14b703728c67074752
Torstein Vik <torsteinv64@gmail.com>
2018-04-17 21:13:31 +0200

    Added FunctionalEquationParameters

--- src/main/scala/MultiplicativeFunction.scala
+++ src/main/scala/MultiplicativeFunction.scala
@@ -23,6 +23,15 @@ case class URI (
 )
 
 
+case class FunctionalEquationParameters (
+    degree: Option[Nat] = None, 
+    conductor: Option[Nat] = None, 
+    signature: Option[(Nat, Nat)] = None, 
+    spectralParameterListR: Option[Seq[ComplexNumber]] = None, 
+    spectralParameterListC: Option[Seq[ComplexNumber]] = None, 
+    sign: Option[ComplexNumber] = None
+)
+
 case class EtaCombination (
     elements: HybridSet[HybridSet[Nat]],
     isProven: Boolean

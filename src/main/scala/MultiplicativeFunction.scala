3055def13f24ebcb9e97dcad3a8fe882be754119
Torstein Vik <torsteinv64@gmail.com>
2018-04-17 21:12:39 +0200

    Added Metadata case cass by spec

--- src/main/scala/MultiplicativeFunction.scala
+++ src/main/scala/MultiplicativeFunction.scala
@@ -3,6 +3,21 @@ package io.github.torsteinvik.zetatypes.db
 import Datatypes._
 
 
+case class Metadata (
+    descriptiveName: String, 
+    verbalDefinition: String, 
+    formalDefinition: Option[String] = None, 
+    latexMacroUnapplied: Option[String] = None, 
+    latexMacroApplied: Option[String] = None, 
+    comments: Seq[String], 
+    firstAddedTimestamp: String, 
+    lastChangedTimestamp: String, 
+    authors: Seq[String], 
+    computationalOrigin: String, 
+    batchId: String, 
+    relatedObjects: Seq[URI] 
+)
+
 case class URI (
     uri : String 
 )

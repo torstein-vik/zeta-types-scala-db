06a0307a290f33075c458d45f68f7fc4b3d5532c
Torstein Vik <torsteinv64@gmail.com>
2018-04-23 00:35:48 +0200

    Made codec package extend Codecs trait, for default implicits

--- src/main/scala/codec/package.scala
+++ src/main/scala/codec/package.scala
@@ -3,4 +3,5 @@ package io.github.torsteinvik.zetatypes.db
 /** This pacakge provides codecs, which transform into and out of JSON
  *  @author Torstein Vik
  */
-package object codec {}
+package object codec extends codec.Codecs { // TODO: remove extension, put into companion object
+} 

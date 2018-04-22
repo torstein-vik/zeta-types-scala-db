d091e7ae66c8e58370d34bfc3cca35826b2c36df
Torstein Vik <torsteinv64@gmail.com>
2018-04-23 00:48:47 +0200

    Removed Encoder and decoder, combined into Codec

--- src/main/scala/codec/Codec.scala
+++ src/main/scala/codec/Codec.scala
@@ -5,18 +5,13 @@ import org.json4s._
 import scala.annotation._
 
 
-@implicitNotFound(msg = "Could not find an encoder for type ${T}")
-trait Encoder[T] {
-    def encode (x : T) : JValue 
-}
 
-@implicitNotFound(msg = "Could not find a decoder for type ${T}")
-trait Decoder[T] {
-    def decode (x : JValue) : T 
-}
 
 @implicitNotFound(msg = "Could not find a codec for type ${T}")
-trait Codec[T] extends Encoder[T] with Decoder[T]
+trait Codec[T] {
+    def encode (x : T) : JValue 
+    def decode (x : JValue) : T 
+}
 
 
 class CodecContainer[T] (val encoder : T => JValue, val decoder : JValue => T) { outer => 

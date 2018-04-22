fbf0b1adb0d4ad08d9a74c902dbd1e049c0bd5af
Torstein Vik <torsteinv64@gmail.com>
2018-04-23 00:36:32 +0200

    Added encode and decode shortcuts in codec package object

--- src/main/scala/codec/package.scala
+++ src/main/scala/codec/package.scala
@@ -1,7 +1,11 @@
 package io.github.torsteinvik.zetatypes.db
 
+import org.json4s._
+
 /** This pacakge provides codecs, which transform into and out of JSON
  *  @author Torstein Vik
  */
 package object codec extends codec.Codecs { // TODO: remove extension, put into companion object
+    def encode[T](x : T)(implicit encoder : Encoder[T]) : JValue = encoder.encode(x)
+    def decode[T](x : JValue)(implicit decoder : Decoder[T]) : T = decoder.decode(x)
 } 

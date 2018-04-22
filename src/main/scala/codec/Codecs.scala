e9d0493e504ff6b2b2f2045962d907e2dce40036
Torstein Vik <torsteinv64@gmail.com>
2018-04-23 00:36:55 +0200

    Added BigInt codec

--- src/main/scala/codec/Codecs.scala
+++ src/main/scala/codec/Codecs.scala
@@ -4,5 +4,14 @@ import org.json4s._
 
 trait Codecs {
     
+    implicit object BigIntCodec extends Codec[BigInt] {
+        // TODO: Note not up to spec, weird [base, 32, [...]] thing not accounted for, seems uneeded
+        def encode (x : BigInt) : JValue = JInt(x)
+        def decode (x : JValue) : BigInt = x match {
+            case JInt(num) => num
+            case _ => throw new CodecException("Found " + x + " expected BigInt (JInt)")
+        }
+    }
+    
     
 }

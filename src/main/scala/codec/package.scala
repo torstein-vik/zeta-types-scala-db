df35c9d8f6ca289392c9b9b99f7edcd52eda05e5
Torstein Vik <torsteinv64@gmail.com>
2018-04-23 00:50:29 +0200

    Removed codec extending Codecs and its implicit hell

--- src/main/scala/codec/package.scala
+++ src/main/scala/codec/package.scala
@@ -5,7 +5,7 @@ import org.json4s._
 /** This pacakge provides codecs, which transform into and out of JSON
  *  @author Torstein Vik
  */
-package object codec extends codec.Codecs { // TODO: remove extension, put into companion object
+package object codec { 
     def encode[T](x : T)(implicit codec : Codec[T]) : JValue = codec.encode(x)
     def decode[T](x : JValue)(implicit codec : Codec[T]) : T = codec.decode(x)
 } 

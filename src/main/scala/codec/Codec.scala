0667e8cf289462648eff1258695e56273c7fbd92
Torstein Vik <torsteinv64@gmail.com>
2018-04-23 00:33:43 +0200

    Split Codec trait into Encoder and Decoder

--- src/main/scala/codec/Codec.scala
+++ src/main/scala/codec/Codec.scala
@@ -2,15 +2,13 @@ package io.github.torsteinvik.zetatypes.db.codec
 
 import org.json4s._
 
-trait Codec[T] {
-    def encode (x : T) : JValue
-    def decode (x : JValue) : T
-}
 trait Encoder[T] {
     def encode (x : T) : JValue 
 }
 trait Decoder[T] {
     def decode (x : JValue) : T 
 }
+
+trait Codec[T] extends Encoder[T] with Decoder[T]
 
 case class CodecException(str : String) extends Exception(str)

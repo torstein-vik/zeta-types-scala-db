42db357d2ff6bb1d855cc0a58d1be3b3e5813d51
Torstein Vik <torsteinv64@gmail.com>
2018-04-23 23:07:26 +0200

    Added Mongo config file

---
+++ src/main/scala/mongo/MongoConfig.scala
@@ -0,0 +1,7 @@
+package io.github.torsteinvik.zetatypes.db.mongo
+
+object MongoConfig {
+    val address         : String = "mongodb://localhost"
+    val dbName          : String = "zetatypesDB"
+    val collectionName  : String = "zetatypes"
+}

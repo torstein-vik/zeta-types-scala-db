da3628ae1ff6a12b1f8b0cba1de65a33250209b8
torstein-vik <torsteinv64@gmail.com>
2018-04-23 09:22:39 +0200

    Added dbstate to REPL

--- src/main/scala/REPL.scala
+++ src/main/scala/REPL.scala
@@ -2,4 +2,10 @@ package io.github.torsteinvik.zetatypes.db
 
 object REPL {
     
+    private object dbstate {
+        private var db : Database = null
+        def getDB = db
+        def setDB (db: Database) = {this.db = db}
+    }
+    
 }

dbeaa9889e2e7a6a70ae570f5d6609a3c1e90165
torstein-vik <torsteinv64@gmail.com>
2018-04-23 09:22:50 +0200

    Added some simple REPL commands

--- src/main/scala/REPL.scala
+++ src/main/scala/REPL.scala
@@ -8,4 +8,9 @@ object REPL {
         def setDB (db: Database) = {this.db = db}
     }
     
+    def use(db : Database) : Unit = {dbstate.setDB(db)}
+    
+    def mfstore(mf : MultiplicativeFunction) : String = ???
+    
+    def mfget(mflabel : String) : MultiplicativeFunction = ???
 }

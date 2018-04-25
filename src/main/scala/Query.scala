d56c375f01f1b16dc01b739fe9dd63813d722e8a
torstein-vik <torsteinv64@gmail.com>
2018-04-25 15:05:01 +0200

    Added combined query

--- src/main/scala/Query.scala
+++ src/main/scala/Query.scala
@@ -1,10 +1,16 @@
 package io.github.torsteinvik.zetatypes.db
 
+import Query._
+
 sealed abstract class Query[T] {
     def map[S](f : T => S) : Query[S] = new MappedQuery[T, S](this, f)
+    def ~[S](query : Query[S]) : Query[T ~ S] = new CombinedQuery[T, S](this, query)
 }
 
 final class MappedQuery[T, S](query : Query[T], map : T => S) extends Query[S]
+final class CombinedQuery[T, S](query1 : Query[T], query2 : Query[S]) extends Query[T ~ S]
 
 object Query {
+    type ~[S, T] = (S, T)
+    
 }

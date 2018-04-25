a2ad1a1ecb1bf54c9cacdb41b419fe7ac96e6d37
torstein-vik <torsteinv64@gmail.com>
2018-04-25 15:04:45 +0200

    Added query mapping

--- src/main/scala/Query.scala
+++ src/main/scala/Query.scala
@@ -1,8 +1,10 @@
 package io.github.torsteinvik.zetatypes.db
 
 sealed abstract class Query[T] {
+    def map[S](f : T => S) : Query[S] = new MappedQuery[T, S](this, f)
 }
 
+final class MappedQuery[T, S](query : Query[T], map : T => S) extends Query[S]
 
 object Query {
 }

b236740e15e6352280f029fea62db143dedc8be5
Torstein Vik <torsteinv64@gmail.com>
2018-04-26 18:01:26 +0200

    Added liftProperty for constant properties

--- src/main/scala/Property.scala
+++ src/main/scala/Property.scala
@@ -6,6 +6,7 @@ trait Property[T] {
 
 trait Properties {
     import scala.language.implicitConversions
+    implicit def liftProperty[T](t : T) : Property[T] = new Property[T]{def get(x : MultiplicativeFunction) = t}
     
     def mf : Property[MultiplicativeFunction] = new Property[MultiplicativeFunction]{def get(x : MultiplicativeFunction) = x}
 }

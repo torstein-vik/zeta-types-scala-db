5bbacba6428acc988047e77d3d485bc7933fc210
Torstein Vik <torsteinv64@gmail.com>
2018-04-27 01:29:47 +0200

    Added Property-queries

--- src/main/scala/query/Query.scala
+++ src/main/scala/query/Query.scala
@@ -5,3 +5,12 @@ import io.github.torsteinvik.zetatypes.db._
 sealed abstract class Query[T] {
 }
 
+
+sealed class PropertyQuery[T] extends Query[T] {
+    def ~[S](query : PropertyQuery[S]) : PropertyQuery[T ~ S] = new CombinedPropertyQuery[T, S](this, query)
+}
+
+final class SinglePropertyQuery[T](property : Property[T]) extends PropertyQuery[T]
+final class CombinedPropertyQuery[T, S](query1 : PropertyQuery[T], query2 : PropertyQuery[S]) extends PropertyQuery[T ~ S]
+
+
--- src/main/scala/query/package.scala
+++ src/main/scala/query/package.scala
@@ -6,4 +6,6 @@ package io.github.torsteinvik.zetatypes.db
 package object query {
     type ~[S, T] = (S, T)
     
+    import scala.language.implicitConversions
+    implicit def propertyAsQuery[T](p : Property[T]) : PropertyQuery[T] = new SinglePropertyQuery[T](p)
 } 

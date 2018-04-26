cc8d673e64d51f407a3472497e025c4a1fe3c510
Torstein Vik <torsteinv64@gmail.com>
2018-04-27 01:30:01 +0200

    Added query filtering

--- src/main/scala/query/Query.scala
+++ src/main/scala/query/Query.scala
@@ -3,8 +3,10 @@ package io.github.torsteinvik.zetatypes.db.query
 import io.github.torsteinvik.zetatypes.db._
 
 sealed abstract class Query[T] {
+    def where(f : Predicate) : Query[T] = new FilteredQuery[T](this, f)
 }
 
+final class FilteredQuery[T](query : Query[T], filter : Predicate) extends Query[T]
 
 sealed class PropertyQuery[T] extends Query[T] {
     def ~[S](query : PropertyQuery[S]) : PropertyQuery[T ~ S] = new CombinedPropertyQuery[T, S](this, query)

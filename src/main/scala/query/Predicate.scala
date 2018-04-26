0f63f74bb7baf5f995623690ad12c06f3ec41b88
Torstein Vik <torsteinv64@gmail.com>
2018-04-27 01:30:34 +0200

    Added basic equality predicate

--- src/main/scala/query/Predicate.scala
+++ src/main/scala/query/Predicate.scala
@@ -3,3 +3,4 @@ package io.github.torsteinvik.zetatypes.db.query
 abstract sealed class Predicate {
 }
 
+sealed case class EqualityPredicate[T](prop1 : Property[T], prop2 : Property[T]) extends Predicate

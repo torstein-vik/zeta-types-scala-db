44e727bce2bce3dd0cd19a8431342c7add0ecfdc
Torstein Vik <torsteinv64@gmail.com>
2018-04-27 01:43:50 +0200

    Added def for downloading and storing OEIS data

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,20 @@ package io.github.torsteinvik.zetatypes.db
 
 object Main extends App with REPL {
     
+    def downloadAndSaveOEIS() {
+        val multsInOEIS : Seq[MultiplicativeFunction] = {
+            import oeis._
+            
+            val download = Download()
+            val conv = download.map(Converter.apply)
+            
+            conv
+        }
+        
+        multsInOEIS.map(mfstore)
+        
+        println(multsInOEIS)
+    }
     
     def DBBellTables() {
         val all = mfgetall

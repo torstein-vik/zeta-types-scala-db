07d877c802c890348f46653b17f1356a0eaea6a9
Torstein Vik <torsteinv64@gmail.com>
2018-04-27 01:44:03 +0200

    Added commented calls to main methods for ease of use

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,12 @@ package io.github.torsteinvik.zetatypes.db
 
 object Main extends App with REPL {
     
+    // Download and store all oeis multiplicative functions in db
+    // downloadAndSaveOEIS()
+    
+    // Print simple bell tables for all multiplicative functions in db
+    // DBBellTables()
+    
     def downloadAndSaveOEIS() {
         val multsInOEIS : Seq[MultiplicativeFunction] = {
             import oeis._

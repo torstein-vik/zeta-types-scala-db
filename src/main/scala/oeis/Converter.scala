e326da0e780f145af1dfeac4bf7f44e008428369
Torstein Vik <torsteinv64@gmail.com>
2018-04-27 01:39:45 +0200

    Added basic json extraction in converter

--- src/main/scala/oeis/Converter.scala
+++ src/main/scala/oeis/Converter.scala
@@ -9,5 +9,13 @@ object Converter{
     private implicit val formats = DefaultFormats
     
     def apply(json : JObject) : MultiplicativeFunction = {
+        val oeisID : String = "A%06d".format((json \ "number").extract[Int])
+        val predata : Seq[BigInt] = (json \ "data").extract[String].split(",").map(BigInt(_))
+        val offset : Int = (json \ "offset").extract[String].split(",")(0).toInt
+        val name : String = (json \ "name").extract[String]
+        val keywords : Seq[(String, Boolean)] = (json \ "keyword").extract[String].split(",").map(s => ("oeis_" + s) -> true)
+        val comments : Seq[String] = (json \ "comment").extract[Seq[String]]
+        val author : String = (json \ "author").extract[String]
+        
     }
 }

1bb883e139611815b2b230e49b8350c83609b96a
Torstein Vik <torsteinv64@gmail.com>
2018-04-27 01:23:54 +0200

    Made store into Unit

--- src/main/scala/Database.scala
+++ src/main/scala/Database.scala
@@ -1,7 +1,7 @@
 package io.github.torsteinvik.zetatypes.db
 
 trait Database {
-    def store(mf : MultiplicativeFunction) : String
+    def store(mf : MultiplicativeFunction) : Unit
     def get(mflabel : String) : MultiplicativeFunction
     def query[T](query : Query[T]) : T
 }

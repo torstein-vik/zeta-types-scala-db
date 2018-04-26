a148c50dede2a8a96fa3730fc82c66cbf1e102f5
Torstein Vik <torsteinv64@gmail.com>
2018-04-27 01:34:05 +0200

    Added central query sender and json parser

--- src/main/scala/oeis/Download.scala
+++ src/main/scala/oeis/Download.scala
@@ -1,9 +1,18 @@
 package io.github.torsteinvik.zetatypes.db.oeis
 
+import scala.io.Source
 
+import org.json4s._
 
 object Download {
     
     def queryurl(i : Int) = "https://oeis.org/search?q=keyword:mult&fmt=json&start=" + (i * 10)
     
+    def query (i : Int) : JValue = {
+        val source = Source.fromURL(queryurl(i))("UTF-8")
+        val result = try source.mkString finally source.close
+        
+        import org.json4s.native.JsonMethods._
+        return parse(result)
+    }
 }

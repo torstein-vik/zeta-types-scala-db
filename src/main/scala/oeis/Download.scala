8ef5017f33b07d484517034c720a425ab80de49d
Torstein Vik <torsteinv64@gmail.com>
2018-04-27 01:34:40 +0200

    Added apply method in Download for managing queries

--- src/main/scala/oeis/Download.scala
+++ src/main/scala/oeis/Download.scala
@@ -6,6 +6,13 @@ import org.json4s._
 
 object Download {
     
+    def apply() : Seq[JObject] = {
+        
+        val data = collection.mutable.ListBuffer[JObject]()
+        return data.to[Seq]
+        
+    }
+    
     def queryurl(i : Int) = "https://oeis.org/search?q=keyword:mult&fmt=json&start=" + (i * 10)
     
     def query (i : Int) : JValue = {

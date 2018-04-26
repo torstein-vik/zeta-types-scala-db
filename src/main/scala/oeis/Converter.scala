6fe676e2e3594a38131d087048998b4298f3b491
Torstein Vik <torsteinv64@gmail.com>
2018-04-27 01:40:14 +0200

    Added bell table construction

--- src/main/scala/oeis/Converter.scala
+++ src/main/scala/oeis/Converter.scala
@@ -8,6 +8,12 @@ import org.json4s._
 object Converter{
     private implicit val formats = DefaultFormats
     
+    //credit: https://stackoverflow.com/questions/8566532/scala-streams-and-their-memory-usage
+    private lazy val naturals: Stream[Int] = Stream.cons(0, naturals.map{_ + 1})
+    
+    //credit: https://gist.github.com/ramn/8378315
+    private lazy val primes: Stream[Int] = 2 #:: Stream.from(3).filter { n => !primes.takeWhile(_ <= math.sqrt(n)).exists(n % _ == 0) }
+    
     def apply(json : JObject) : MultiplicativeFunction = {
         val oeisID : String = "A%06d".format((json \ "number").extract[Int])
         val predata : Seq[BigInt] = (json \ "data").extract[String].split(",").map(BigInt(_))
@@ -24,6 +30,10 @@ object Converter{
             case 2 => BigInt(1) +: BigInt(1) +: predata
             case n => throw new Exception("Weird OEIS offset at " + oeisID + ": " + n)
         }
+        
+        val bellTable : List[(Prime, List[Integer])] = (for (p <- primes.takeWhile(_ < data.length)) yield Prime(p) -> (
+            for (e <- naturals.takeWhile(math.pow(p, _) < data.length)) yield Integer(data(math.pow(p, e).toInt))
+        ).toList).toList
         
     }
 }

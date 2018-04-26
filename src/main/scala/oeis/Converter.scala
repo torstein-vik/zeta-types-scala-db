12add0da310382b1a5fa82ecd71de6d50578a13d
Torstein Vik <torsteinv64@gmail.com>
2018-04-27 01:40:02 +0200

    Added offset manager in converter

--- src/main/scala/oeis/Converter.scala
+++ src/main/scala/oeis/Converter.scala
@@ -17,5 +17,13 @@ object Converter{
         val comments : Seq[String] = (json \ "comment").extract[Seq[String]]
         val author : String = (json \ "author").extract[String]
         
+        val data : Seq[BigInt] = offset match {
+            case k if k < 0 => predata.drop( - k)
+            case 0 => predata
+            case 1 => BigInt(1) +: predata
+            case 2 => BigInt(1) +: BigInt(1) +: predata
+            case n => throw new Exception("Weird OEIS offset at " + oeisID + ": " + n)
+        }
+        
     }
 }

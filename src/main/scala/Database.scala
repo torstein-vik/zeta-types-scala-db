ce45afc2db9547b51bdc05e34383bf6394fc8864
Torstein Vik <torsteinv64@gmail.com>
2018-04-27 01:24:18 +0200

    Added length and getAll def to DB

--- src/main/scala/Database.scala
+++ src/main/scala/Database.scala
@@ -1,8 +1,13 @@
 package io.github.torsteinvik.zetatypes.db
 
+import query._
+
 trait Database {
     def batch(mfs : Seq[MultiplicativeFunction], batchid : String = null) : Unit
     def store(mf : MultiplicativeFunction) : Unit
     def get(mflabel : String) : MultiplicativeFunction
     def query[T](query : Query[T]) : T
+    
+    def length : Int
+    def getAll : Seq[MultiplicativeFunction]
 }

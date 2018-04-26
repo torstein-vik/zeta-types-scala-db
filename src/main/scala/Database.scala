519127f93421c019aed755b749f619b317ae69be
Torstein Vik <torsteinv64@gmail.com>
2018-04-27 01:24:02 +0200

    Added batch def

--- src/main/scala/Database.scala
+++ src/main/scala/Database.scala
@@ -1,6 +1,7 @@
 package io.github.torsteinvik.zetatypes.db
 
 trait Database {
+    def batch(mfs : Seq[MultiplicativeFunction], batchid : String = null) : Unit
     def store(mf : MultiplicativeFunction) : Unit
     def get(mflabel : String) : MultiplicativeFunction
     def query[T](query : Query[T]) : T

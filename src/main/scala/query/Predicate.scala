5cb11e69041b08fce606bb55d555043f856cc77d
Torstein Vik <torsteinv64@gmail.com>
2018-04-27 01:30:47 +0200

    Added And and Or predicate combinators

--- src/main/scala/query/Predicate.scala
+++ src/main/scala/query/Predicate.scala
@@ -1,6 +1,11 @@
 package io.github.torsteinvik.zetatypes.db.query
 
 abstract sealed class Predicate {
+    def and (other : Predicate) = new AndPredicate(this, other)
+    def or (other : Predicate) = new OrPredicate(this, other)
 }
 
 sealed case class EqualityPredicate[T](prop1 : Property[T], prop2 : Property[T]) extends Predicate
+sealed case class AndPredicate(pred1 : Predicate, pred2 : Predicate) extends Predicate
+sealed case class OrPredicate(pred1 : Predicate, pred2 : Predicate) extends Predicate
+

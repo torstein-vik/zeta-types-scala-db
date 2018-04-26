8fc05657d1d93de69765e2a20723c8284f15e107
Torstein Vik <torsteinv64@gmail.com>
2018-04-27 01:31:56 +0200

    Added === in Property for Predicate DSL

--- src/main/scala/query/Property.scala
+++ src/main/scala/query/Property.scala
@@ -3,6 +3,7 @@ package io.github.torsteinvik.zetatypes.db.query
 import io.github.torsteinvik.zetatypes.db._
 
 abstract sealed class Property[T] {
+    def === (other : Property[T]) : Predicate = new EqualityPredicate[T](this, other)
 }
 
 

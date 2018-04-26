f2a5f1557a83314609fb667b87f5b7a5e5b6a9a8
Torstein Vik <torsteinv64@gmail.com>
2018-04-27 01:28:54 +0200

    Added ~ type to query package

--- src/main/scala/query/package.scala
+++ src/main/scala/query/package.scala
@@ -3,4 +3,7 @@ package io.github.torsteinvik.zetatypes.db
 /** This pacakge provides dsl for creating queries, as well as a naive query system
  *  @author Torstein Vik
  */
-package object query {} 
+package object query {
+    type ~[S, T] = (S, T)
+    
+} 

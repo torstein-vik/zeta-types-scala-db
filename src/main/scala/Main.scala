0df5ae505cf598c103ae30e40d52afce3cc48eb2
Torstein Vik <torsteinv64@gmail.com>
2018-04-27 01:43:33 +0200

    Added def for printing bell tables

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,4 +3,10 @@ package io.github.torsteinvik.zetatypes.db
 object Main extends App with REPL {
     
     
+    def DBBellTables() {
+        val all = mfgetall
+        
+        all foreach {mf => println(mf.bellTableText()); println()}
+    }
+    
 }

be31fc26f6b77c7efc4329fc6ce848c559e01ca9
Torstein Vik <torsteinv64@gmail.com>
2018-04-26 18:52:20 +0200

    Added package for query stuff

---
+++ src/main/scala/query/package.scala
@@ -0,0 +1,6 @@
+package io.github.torsteinvik.zetatypes.db
+
+/** This pacakge provides dsl for creating queries, as well as a naive query system
+ *  @author Torstein Vik
+ */
+package object query {} 

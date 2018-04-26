76e767de78bf535feed3a7bac395259a88cba16d
Torstein Vik <torsteinv64@gmail.com>
2018-04-26 18:03:27 +0200

    Moved MappedQuery and CombinedQuery into comp ob

--- src/main/scala/Query.scala
+++ src/main/scala/Query.scala
@@ -7,10 +7,12 @@ sealed abstract class Query[T] {
     def ~[S](query : Query[S]) : Query[T ~ S] = new CombinedQuery[T, S](this, query)
 }
 
-final class MappedQuery[T, S](query : Query[T], map : T => S) extends Query[S]
-final class CombinedQuery[T, S](query1 : Query[T], query2 : Query[S]) extends Query[T ~ S]
-
 object Query {
     type ~[S, T] = (S, T)
+    final class MappedQuery[T, S](query : Query[T], map : T => S) extends Query[S]
+    
+    
+    final class CombinedQuery[T, S](query1 : Query[T], query2 : Query[S]) extends Query[T ~ S]
+
     
 }

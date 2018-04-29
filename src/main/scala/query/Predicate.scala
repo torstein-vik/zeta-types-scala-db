8336645a36b186af6fe7edab771e385f6fbd570e
Torstein Vik <torsteinv64@gmail.com>
2018-04-30 01:22:05 +0200

    Unsealed predicate case classes, as these are auto final

--- src/main/scala/query/Predicate.scala
+++ src/main/scala/query/Predicate.scala
@@ -12,14 +12,14 @@ abstract sealed class Predicate {
     final def | (other : Predicate) = or(other)
 }
 
-sealed case class EqualityPredicate[T](prop1 : Property[T], prop2 : Property[T]) extends Predicate
-sealed case class StringContainsPredicate(superstr : Property[String], substr : Property[String]) extends Predicate
-sealed case class RegexPredicate(str : Property[String], regex : Regex) extends Predicate
+case class EqualityPredicate[T](prop1 : Property[T], prop2 : Property[T]) extends Predicate
+case class StringContainsPredicate(superstr : Property[String], substr : Property[String]) extends Predicate
+case class RegexPredicate(str : Property[String], regex : Regex) extends Predicate
 
-sealed case class SeqContainsPredicate[T](seq : Property[Seq[T]], element : Property[T]) extends Predicate
-sealed case class SeqHasPredicate[T](seq : Property[Seq[T]], pred : Property[T] => Predicate) extends Predicate
-sealed case class SeqAllPredicate[T](seq : Property[Seq[T]], pred : Property[T] => Predicate) extends Predicate
+case class SeqContainsPredicate[T](seq : Property[Seq[T]], element : Property[T]) extends Predicate
+case class SeqHasPredicate[T](seq : Property[Seq[T]], pred : Property[T] => Predicate) extends Predicate
+case class SeqAllPredicate[T](seq : Property[Seq[T]], pred : Property[T] => Predicate) extends Predicate
 
-sealed case class AndPredicate(pred1 : Predicate, pred2 : Predicate) extends Predicate
-sealed case class OrPredicate(pred1 : Predicate, pred2 : Predicate) extends Predicate
-sealed case class NotPredicate(pred : Predicate) extends Predicate
+case class AndPredicate(pred1 : Predicate, pred2 : Predicate) extends Predicate
+case class OrPredicate(pred1 : Predicate, pred2 : Predicate) extends Predicate
+case class NotPredicate(pred : Predicate) extends Predicate

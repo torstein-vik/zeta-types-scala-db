7deec05e9d3b957666a37d3fde8fecf7d78f2494
Torstein Vik <torsteinv64@gmail.com>
2018-04-30 00:25:00 +0200

    Added not-predicate

--- src/main/scala/query/Predicate.scala
+++ src/main/scala/query/Predicate.scala
@@ -3,9 +3,11 @@ package io.github.torsteinvik.zetatypes.db.query
 abstract sealed class Predicate {
     def and (other : Predicate) = new AndPredicate(this, other)
     def or (other : Predicate) = new OrPredicate(this, other)
+    def not = new NotPredicate(this)
+    
 }
 
 sealed case class EqualityPredicate[T](prop1 : Property[T], prop2 : Property[T]) extends Predicate
 sealed case class AndPredicate(pred1 : Predicate, pred2 : Predicate) extends Predicate
 sealed case class OrPredicate(pred1 : Predicate, pred2 : Predicate) extends Predicate
-
+sealed case class NotPredicate(pred : Predicate) extends Predicate

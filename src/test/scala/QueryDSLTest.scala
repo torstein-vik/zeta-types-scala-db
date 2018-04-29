5e00b9db2e994a91fac3c478cd370dd0ef95a91f
Torstein Vik <torsteinv64@gmail.com>
2018-04-30 00:32:34 +0200

    Changed mf ~ mflabel ~ mf to ... ~ mfvalue(2) as a better examples

--- src/test/scala/QueryDSLTest.scala
+++ src/test/scala/QueryDSLTest.scala
@@ -3,13 +3,14 @@ package io.github.torsteinvik.zetatypes.test.db
 import org.scalatest.FunSuite
 
 import io.github.torsteinvik.zetatypes.db._
+import io.github.torsteinvik.zetatypes.db.Datatypes._
 import io.github.torsteinvik.zetatypes.db.query._
 import io.github.torsteinvik.zetatypes.db.query.Property._
 
 class QueryDSLTest extends FunSuite {
     test("basic queries") {
         mf : Query[MultiplicativeFunction]
-        mf ~ mflabel ~ mf : Query[MultiplicativeFunction ~ String ~ MultiplicativeFunction]
+        mf ~ mflabel ~ mfvalue(2) : Query[MultiplicativeFunction ~ String ~ ComplexNumber]
         
         mflabel ~ mf where mf === mf : Query[String ~ MultiplicativeFunction]
         mflabel ~ mf where (mf === mf and mf === mf) : Query[String ~ MultiplicativeFunction]

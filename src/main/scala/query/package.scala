19e41a3e31f7fdf7ccddb260168a9fbeabfaf69a
Torstein Vik <torsteinv64@gmail.com>
2018-05-02 03:35:53 +0200

    Fixed packagedoc

--- src/main/scala/query/package.scala
+++ src/main/scala/query/package.scala
@@ -1,6 +1,6 @@
 package io.github.torsteinvik.zetatypes.db
 
-/** This pacakge provides dsl for creating queries, as well as a naive query system
+/** This pacakge provides dsl for creating queries, as well as a direct querying system
  *  @author Torstein Vik
  */
 package object query {

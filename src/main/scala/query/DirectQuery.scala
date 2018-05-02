93ec955973956e97bdfd6927089e978aa4a7fe05
Torstein Vik <torsteinv64@gmail.com>
2018-05-02 03:51:18 +0200

    Added shells for evalProperty and evalPredicate

--- src/main/scala/query/DirectQuery.scala
+++ src/main/scala/query/DirectQuery.scala
@@ -7,5 +7,12 @@ object DirectQuery {
     def query[T](q : Query[T])(mfs : Seq[MultiplicativeFunction]) : QueryResult[T] = new QueryResult(q match {
     })
     
+    def evalProperty[T](p : Property[T], mf : MultiplicativeFunction) : T = p match {
+    }
+    
+    def evalPredicate(p : Predicate, mf : MultiplicativeFunction) : Boolean = p match {
+    }
+    
+    
     
 }

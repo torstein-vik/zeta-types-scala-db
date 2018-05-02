676fa02e91ae2901b36bd839dadbd58fdceda673
Torstein Vik <torsteinv64@gmail.com>
2018-05-02 03:51:43 +0200

    Implemented ConstantProperty and GetProperty eval

--- src/main/scala/query/DirectQuery.scala
+++ src/main/scala/query/DirectQuery.scala
@@ -8,6 +8,11 @@ object DirectQuery {
     })
     
     def evalProperty[T](p : Property[T], mf : MultiplicativeFunction) : T = p match {
+        case ConstantProperty(x) => x
+        case GetProperty(x) => evalProperty[Option[T]](x, mf) match {
+            case Some(y) => y
+            case None => throw new Exception("Value assumed to exist in q did not exist!")
+        }
     }
     
     def evalPredicate(p : Predicate, mf : MultiplicativeFunction) : Boolean = p match {

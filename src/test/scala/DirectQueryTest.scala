2557b897973fa04791179ee131dbc90c4b0c5fde
Torstein Vik <torsteinv64@gmail.com>
2018-05-02 03:44:41 +0200

    Added shell for test suite for direct querying

---
+++ src/test/scala/DirectQueryTest.scala
@@ -0,0 +1,11 @@
+package io.github.torsteinvik.zetatypes.test.db
+
+import org.scalatest.FunSuite
+
+import io.github.torsteinvik.zetatypes.db._
+import io.github.torsteinvik.zetatypes.db.Datatypes._
+import io.github.torsteinvik.zetatypes.db.query._
+import io.github.torsteinvik.zetatypes.db.query.Property._
+
+class DirectQueryTest extends FunSuite {
+}

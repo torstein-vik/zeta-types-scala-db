9654894f919b32ce7a807271c4889e2dd365a2a3
Torstein Vik <torsteinv64@gmail.com>
2018-05-02 03:52:05 +0200

    Implemented basic MFProperties

--- src/main/scala/query/DirectQuery.scala
+++ src/main/scala/query/DirectQuery.scala
@@ -13,6 +13,17 @@ object DirectQuery {
             case Some(y) => y
             case None => throw new Exception("Value assumed to exist in q did not exist!")
         }
+        case p : MFProperty[T] => p match {
+            case Property.mf => mf
+            case Property.mflabel => mf.mflabel
+            case Property.batchid => mf.metadata.batchId
+            case Property.name => mf.metadata.descriptiveName
+            case Property.belltable => mf.bellTableText
+            case Property.definition => mf.metadata.verbalDefinition
+            case Property.comments => mf.metadata.comments
+            case Property.properties => mf.properties.entries.collect{ case (property, true) => property }
+            
+        }
     }
     
     def evalPredicate(p : Predicate, mf : MultiplicativeFunction) : Boolean = p match {

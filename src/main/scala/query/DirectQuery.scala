1659b521e9a1d6bfc2f7b030637ead964f8f6ef3
Torstein Vik <torsteinv64@gmail.com>
2018-05-06 16:34:47 +0200

    Moved mfvalue factorisation into definition of mfvalue property

--- src/main/scala/query/DirectQuery.scala
+++ src/main/scala/query/DirectQuery.scala
@@ -4,7 +4,6 @@ import io.github.torsteinvik.zetatypes.db._
 import io.github.torsteinvik.zetatypes.db.query.Property._
 import io.github.torsteinvik.zetatypes.db.Datatypes._
 
-import scala.annotation.tailrec
 
 object DirectQuery {
     
@@ -33,11 +32,11 @@ object DirectQuery {
             
             case belltable(ps, es) => mf.bellTableText(ps, es)
             case mfbell(p, Nat(e)) => mf.bellTable.values.find(_._1 == p).map(_._2.lift(e.toInt)).flatten
-            case mfvalue(nn @ Nat(n)) => n match {
+            case nn @ mfvalue(Nat(n)) => n match {
                 case _ if n == 0 => Some(Nat(0)) 
                 case _ if n == 1 => mf.bellTable.values.headOption.map(_._2.headOption).flatten // In all cases but one, this is 1. Discuss this counterexample, should it be included?
                 case _ => {
-                    val parts = factor(nn).map(mfbell.tupled).map(evalProperty[Option[ComplexNumber]](_, mf))
+                    val parts = nn.factors.map(mfbell.tupled).map(evalProperty[Option[ComplexNumber]](_, mf))
                     if (parts.exists(_ isEmpty)) None else Some(parts.map(_.get).foldLeft[ComplexNumber](Integer(1))({
                         case (acc, next) => CartesianComplex(Floating(acc.re * next.re - acc.im * next.im), Floating(acc.re * next.im + acc.im * next.re))
                     }))
@@ -60,25 +59,6 @@ object DirectQuery {
         case AndPredicate(pred1, pred2) => evalPredicate(pred1, mf) && evalPredicate(pred2, mf)
         case OrPredicate(pred1, pred2) => evalPredicate(pred1, mf) || evalPredicate(pred2, mf)
         case NotPredicate(pred) => !evalPredicate(pred, mf)
-    }
-    
-    //TODO: unify with other primes and naturals stream
-    //credit: https://gist.github.com/ramn/8378315
-    private lazy val primes: Stream[Int] = 2 #:: Stream.from(3).filter { n => !primes.takeWhile(_ <= math.sqrt(n)).exists(n % _ == 0) }
-    //credit: https://stackoverflow.com/questions/8566532/scala-streams-and-their-memory-usage
-    private lazy val naturals: Stream[Int] = Stream.cons(0, naturals.map{_ + 1})
-    
-    // TODO: move this elsewhere...
-    private def factor(nat : Nat) : Seq[(Prime, Nat)] = {
-        @tailrec
-        def factor_(n : BigInt, curindex : Int, seq : Seq[(Int, Int)]) : Seq[(Int, Int)] = {
-            if (n == BigInt(1)) return seq
-            val (p : Int, i : Int) = primes.zipWithIndex.drop(curindex).find{n % _._1 == BigInt(0)}.get
-            val e : Int = naturals.find(e => n % BigInt(p).pow(e) > 0).get - 1
-            factor_(n / BigInt(p).pow(e), i + 1, (p, e) +: seq)
-        }
-        
-        factor_(nat.x, 0, Seq()).map{case (p, e) => Prime(p) -> Nat(e)}
     }
     
 }

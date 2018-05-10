f9254a97135347d83a16e83b3b4c00d8d5448bdc
Torstein Vik <torsteinv64@gmail.com>
2018-05-11 00:19:25 +0200

    Added user friendly parse method

--- src/main/scala/query/parsing/Parser.scala
+++ src/main/scala/query/parsing/Parser.scala
@@ -15,4 +15,10 @@ object Parser extends RegexParsers {
     case class ParserException(msg : String, pos : Position) extends 
         Exception("Parsing error at line " + pos.line + " column " + pos.column + "\n" + pos.longString + "\n" + msg)
     
+    def parse[T] (str : String)(implicit parser : Parser[T]) : T = parseAll(parser, str) match {
+        case Success(result, _) => result
+        case Failure(msg, next) => throw ParserException(msg, next.pos)
+        case Error(msg, next) => throw ParserException(msg, next.pos)
+    }
+    
 }

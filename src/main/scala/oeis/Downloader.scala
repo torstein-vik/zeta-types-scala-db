ca1425ef7a90381b87914fc734bfec231d673341
Torstein Vik <torsteinv64@gmail.com>
2018-05-11 01:23:59 +0200

    Cleaned up downloader somewhat

--- src/main/scala/oeis/Downloader.scala
+++ src/main/scala/oeis/Downloader.scala
@@ -24,17 +24,13 @@ object Downloader {
     private def runDownloader() : Unit = {
         Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
         while(true){
-            Thread.sleep(300)
-            //println("Queue size: " + downloadQueue.length) // For some reason this line is essential. What the fuck.
-            if(!downloadQueue.isEmpty) handleRequest()
-            if(!downloadQueue.isEmpty) handleRequest()
-            if(!downloadQueue.isEmpty) handleRequest()
+            Thread.sleep(500)
             if(!downloadQueue.isEmpty) handleRequest()
         }
     }
     
     private def handleRequest() : Unit = {
-        val (url : String, promise : Promise[Seq[String]]) = /*if(downloadQueue.length > 200) downloadQueue.dequeueFirst(_._1) else*/ downloadQueue.dequeue()
+        val (url : String, promise : Promise[Seq[String]]) = downloadQueue.dequeue()
         println("Downloading " + url)
         val source = Source.fromURL(url)("UTF-8") 
         val download = Try(source.getLines.toList) 

2e0faa1f775eb7b139b21f509c749230e5bece94
Torstein Vik <torsteinv64@gmail.com>
2018-05-11 01:34:26 +0200

    Removed Main.DBBellTables, it's purpose is long gone

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,19 +7,9 @@ object Main extends App with REPL {
     // Download and store all oeis multiplicative functions in db, and use b-files for extra data
     // oeis.Manager(mfstore, useBFile = true)
     
-    // Print simple bell tables for all multiplicative functions in db
-    // DBBellTables()
-    
     // All multiplicative functions where 24 and 51 are fix-points
     // mfquery(pretty() where (mfvalue(24) ==? 24 and mfvalue(51) ==? 51)).print()
-    
-    def DBBellTables() {
-        val all = mfgetall
         
-        // TODO: Do this with query
-        //all foreach {mf => println(mf.bellTableText()); println()}
-    }
-    
     // Close database
     close()
 }

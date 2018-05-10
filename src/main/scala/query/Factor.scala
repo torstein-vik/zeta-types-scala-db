98871882c8e801f7a4a28e2dc6d3a11b471611a0
Torstein Vik <torsteinv64@gmail.com>
2018-05-10 23:06:59 +0200

    Cleaned up Factor.scala a bit

--- src/main/scala/query/Factor.scala
+++ src/main/scala/query/Factor.scala
@@ -8,16 +8,12 @@ import scala.annotation.tailrec
 // TODO: This really needs to be placed somewhere more fitting...
 object Factor {
     
-    //credit: https://stackoverflow.com/questions/8566532/scala-streams-and-their-memory-usage
-    private lazy val naturals: Stream[Int] = Stream.cons(0, naturals.map{_ + 1})
-    
-    // TODO: move this elsewhere...
     def apply(nat : Nat) : Seq[(Prime, Nat)] = {
         @tailrec
         def factor_(n : BigInt, curindex : Int, seq : Seq[(Int, Int)]) : Seq[(Int, Int)] = {
             if (n == BigInt(1)) return seq
             val (p : Int, i : Int) = primes.zipWithIndex.drop(curindex).find{n % _._1 == BigInt(0)}.get
-            val e : Int = naturals.find(e => n % BigInt(p).pow(e) > 0).get - 1
+            val e : Int = Stream.from(0).find(e => n % BigInt(p).pow(e) > 0).get - 1
             factor_(n / BigInt(p).pow(e), i + 1, (p, e) +: seq)
         }
         

cee20c8b73760585525d7cfa6065b747ce39d836
Torstein Vik <torsteinv64@gmail.com>
2018-05-11 00:19:06 +0200

    Added object for all parsers

---
+++ src/main/scala/query/parsing/Parser.scala
@@ -0,0 +1,15 @@
+package io.github.torsteinvik.zetatypes.db.query.parsing
+
+import io.github.torsteinvik.zetatypes.db._
+import io.github.torsteinvik.zetatypes.db.Datatypes._
+import io.github.torsteinvik.zetatypes.db.query._
+import io.github.torsteinvik.zetatypes.db.query.Property._
+
+import scala.util.parsing.combinator._
+import scala.util.parsing.input._
+
+import scala.reflect.runtime.universe.{Position => _, _}
+
+object Parser extends RegexParsers {
+    
+}

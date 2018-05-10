b4ff03a3ff6b60a9c1d59512f800e978eaca2184
Torstein Vik <torsteinv64@gmail.com>
2018-05-10 23:52:19 +0200

    Added computation to minimize query requirements

--- src/main/scala/query/Requirements.scala
+++ src/main/scala/query/Requirements.scala
@@ -1,5 +1,8 @@
 package io.github.torsteinvik.zetatypes.db.query
 
+import io.github.torsteinvik.zetatypes.db._
+import io.github.torsteinvik.zetatypes.db.query.Property._
+import io.github.torsteinvik.zetatypes.db.Datatypes._
 
 final class Requirements(requirements : Set[MFProperty[_]]) {
     
@@ -16,5 +19,17 @@ final class Requirements(requirements : Set[MFProperty[_]]) {
     The latter two not implemented here
     
     */
+    
+    // TODO: Use isDefinedAt to do this automatically
+    val minimal : Set[MFProperty[_]] = requirements match {
+        case reqs if reqs.contains(mf) => Set(mf)
+        case reqs if reqs.contains(belltable) => reqs.filter({
+            case _ : bellcell => false
+            case _ : bellrow => false
+            case _ : bellsmalltable => false
+            case _ => true
+        })
+        case reqs => reqs
+    }
     
 }

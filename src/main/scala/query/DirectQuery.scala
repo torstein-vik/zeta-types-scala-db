becafd3f234e8b0986f526214fd75c32c39fa9ff
Torstein Vik <torsteinv64@gmail.com>
2018-05-10 23:50:01 +0200

    Updated DirectQuery to use providers

--- src/main/scala/query/DirectQuery.scala
+++ src/main/scala/query/DirectQuery.scala
@@ -4,14 +4,13 @@ import io.github.torsteinvik.zetatypes.db._
 import io.github.torsteinvik.zetatypes.db.query.Property._
 import io.github.torsteinvik.zetatypes.db.Datatypes._
 
-
 object DirectQuery {
     
     private[DirectQuery] sealed abstract class EvalContext
     case object NoContext extends EvalContext
     case class LambdaContext[T](t : T) extends EvalContext
     
-    def query[T](q : Query[T])(mfs : Seq[MultiplicativeFunction]) : QueryResult[T] = new QueryResult(q match {
+    def query[T](q : Query[T])(mfs : Seq[MFPropertyProvider]) : QueryResult[T] = new QueryResult(q match {
         case q : PropertyQuery[T] => q match {
             case CombinedPropertyQuery(q1, q2) => (query(q1)(mfs) zip query(q2)(mfs)) map {case (x, y) => x ~ y}
             case SinglePropertyQuery(property) => mfs.map(evalProperty(property, _)(NoContext))
@@ -19,18 +18,8 @@ object DirectQuery {
         case FilteredQuery(innerq, predicate) => query(innerq)(mfs.filter(evalPredicate(predicate, _)(NoContext)))
     })
     
-    def evalProperty[T](p : Property[T], mf : MultiplicativeFunction)(implicit ctx : EvalContext) : T = p match {
-        case p : MFProperty[T] => p match {
-            case Property.mf => mf
-            case Property.mflabel => mf.mflabel
-            case Property.batchid => mf.metadata.batchId
-            case Property.name => mf.metadata.descriptiveName
-            case Property.definition => mf.metadata.verbalDefinition
-            case Property.comments => mf.metadata.comments
-            case Property.properties => mf.properties
-            
-            case bellcell(p, Nat(e)) => mf.bellTable.values.find(_._1 == p).map(_._2.lift(e.toInt)).flatten
-        }
+    def evalProperty[T](p : Property[T], mf : MFPropertyProvider)(implicit ctx : EvalContext) : T = p match {
+        case p : MFProperty[T] => mf.provide(p)
         
         case p : CompoundProperty[T] => p match {
             case LambdaInputProperty() => ctx match {
@@ -46,17 +35,17 @@ object DirectQuery {
             case TupleFirstProperty(tuple) => evalProperty(tuple, mf)._1
             case TupleSecondProperty(tuple) => evalProperty(tuple, mf)._2
             case pretty(ps, es) => {
-                var str = "Label: " + mflabel + "\t Name: " + metadata.descriptiveName + "\n Description: " + metadata.verbalDefinition + "\n\n Bell Table: \n"
+                var str = "Label: " + mf.provide(mflabel) + "\t Name: " + mf.provide(name) + "\n Description: " + mf.provide(definition) + "\n\n Bell Table: \n"
                 
-                for {(Prime(prime), vals : Seq[ComplexNumber]) <- bellTable.values.take(ps)} { // max ps primes
-                    str = str + "\np=" + prime + ": \t "+ vals.take(es).map(_.pretty).mkString(",\t") // max es exponents 
+                for {(Prime(prime), vals : Seq[ComplexNumber]) <- mf.provide(bellsmalltable(ps, es))} {
+                    str = str + "\np=" + prime + ": \t "+ vals.map(_.pretty).mkString(",\t")
                 }
                 
                 return str
             }
             case nn @ mfvalue(Nat(n)) => n match {
                 case _ if n == 0 => Some(Nat(0)) 
-                case _ if n == 1 => mf.bellTable.values.headOption.map(_._2.headOption).flatten // In all cases but one, this is 1. Discuss this counterexample, should it be included?
+                case _ if n == 1 => mf.provide(bellcell(Prime(2), Nat(0))) // In all cases but one, this is 1. Discuss this counterexample, should it be included?
                 case _ => {
                     val parts = nn.factors.map(evalProperty(_, mf))
                     if (parts.exists(_ isEmpty)) None else Some(parts.map(_.get).foldLeft[ComplexNumber](Integer(1))({
@@ -67,7 +56,7 @@ object DirectQuery {
         }
     }
     
-    def evalPredicate(p : Predicate, mf : MultiplicativeFunction)(implicit ctx : EvalContext) : Boolean = p match {
+    def evalPredicate(p : Predicate, mf : MFPropertyProvider)(implicit ctx : EvalContext) : Boolean = p match {
         case EqualityPredicate(prop1, prop2) => evalProperty(prop1, mf) == evalProperty(prop2, mf)
         case StringContainsPredicate(superstr, substr) => evalProperty(superstr, mf) contains evalProperty(substr, mf)
         case RegexPredicate(str, regex) => evalProperty(str, mf) match {case regex() => true case _ => false}
@@ -85,6 +74,6 @@ object DirectQuery {
         case BooleanPredicate(prop) => evalProperty(prop, mf)
     }
     
-    def evalPropertyLambda[T](lambda : PropertyLambda[T], v : T, mf : MultiplicativeFunction) : Boolean = evalPredicate(lambda.output, mf)(LambdaContext[T](v))
+    def evalPropertyLambda[T](lambda : PropertyLambda[T], v : T, mf : MFPropertyProvider) : Boolean = evalPredicate(lambda.output, mf)(LambdaContext[T](v))
     
 }

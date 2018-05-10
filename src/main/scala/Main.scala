c92753e9163eed71634382bc2753bed464f512ca
Torstein Vik <torsteinv64@gmail.com>
2018-05-11 01:39:03 +0200

    Added TODO list to Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,6 +4,22 @@ object Main extends App with REPL {
     
     import query.Property._
     
+    /* TODO:
+    1. Speed up belltable-generation in OEIS converter
+       - Implement better prime sieve
+       - Debug the other time-sinks
+    
+    2. Get the OEIS downloader working
+       - There appear to be memory leaks
+    
+    3. Test out queries on the large OEIS db for debug and benchmark purposes
+    
+    4. Get the mongo property-provider to only encode the bell-table values that are needed, this is a huge time-sink
+    
+    5. Get the mongo query system to only retrive what is needed, using projections
+    
+    */
+    
     // Download and store all oeis multiplicative functions in db, and use b-files for extra data
     // oeis.Manager(mfstore, useBFile = true)
     

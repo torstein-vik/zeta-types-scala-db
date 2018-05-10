b152e72ac6113bc0ad1ae74796d02fd18a8ef213
Torstein Vik <torsteinv64@gmail.com>
2018-05-11 00:18:18 +0200

    Added test for parsing mfproperties

--- src/test/scala/ParserTest.scala
+++ src/test/scala/ParserTest.scala
@@ -9,6 +9,22 @@ import io.github.torsteinvik.zetatypes.db._
 
 class ParserTest extends FunSuite {
     
+    test ("Parsing mfproperties") {
+        
+        assert(parse("mf")(mfproperty[MultiplicativeFunction]) == mf)
+        assert(parse("batchid")(mfproperty[Option[String]]) == batchid)
+        assert(parse("mflabel")(mfproperty[String]) == mflabel)
+        assert(parse("name")(mfproperty[String]) == name)
+        assert(parse("definition")(mfproperty[String]) == definition)
+        assert(parse("comments")(mfproperty[Seq[String]]) == comments)
+        assert(parse("properties")(mfproperty[Record[Boolean]]) == properties)
+        assert(parse("belltable")(mfproperty[Seq[(Prime, Seq[ComplexNumber])]]) == belltable)
+        
+        assert(parse("bellcell(11, 34)")(mfproperty[Option[ComplexNumber]]) == bellcell(11, 34))
+        assert(parse("bellrow(7)")(mfproperty[Option[Seq[ComplexNumber]]]) == bellrow(7))
+        assert(parse("bellsmalltable(12, 12)")(mfproperty[Seq[(Prime, Seq[ComplexNumber])]]) == bellsmalltable(12, 12))
+        
+    }
     
     test ("Parsing literals") {
         assert(parse("342")(literal[Int]) === 342)

2941ee1e61283d89f22d92c5d9467edeacea5102
Torstein Vik <torsteinv64@gmail.com>
2018-05-11 00:18:06 +0200

    Added test for parsing literals

--- src/test/scala/ParserTest.scala
+++ src/test/scala/ParserTest.scala
@@ -10,4 +10,17 @@ import io.github.torsteinvik.zetatypes.db._
 class ParserTest extends FunSuite {
     
     
+    test ("Parsing literals") {
+        assert(parse("342")(literal[Int]) === 342)
+        assert(parse("-342")(literal[Int])  === -342)
+        assert(parse("342")(literal[Integer]) === Integer(342))
+        assert(parse("-342")(literal[Integer]) === Integer(-342))
+        assert(parse("12")(literal[Nat]) === Nat(12))
+        assert(parse("53")(literal[Nat]) === Nat(53))
+        assert(parse("17")(literal[Prime]) === Prime(17))
+        assert(parse("3")(literal[Prime]) === Prime(3))
+        assert(parse("""" Hey """")(literal[String]) === " Hey ")
+        assert(parse("""" Hey \" \r \\"""")(literal[String])  === " Hey \" \r \\")
+    }
+    
 }

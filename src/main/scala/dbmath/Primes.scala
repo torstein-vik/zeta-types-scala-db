49d97e59c6423da21c3d351146134d538ad4ea63
Torstein Vik <torsteinv64@gmail.com>
2018-05-11 01:25:52 +0200

    Added note about current prime sieve, fix later

--- src/main/scala/dbmath/Primes.scala
+++ src/main/scala/dbmath/Primes.scala
@@ -1,5 +1,6 @@
 package io.github.torsteinvik.zetatypes.db.dbmath
 
 object Primes {
+    // TODO: This is very slow, speed up
     lazy val primes: Stream[Int] = 2 #:: Stream.from(3, 2).filter { n => !primes.takeWhile(_ <= math.sqrt(n)).exists(n % _ == 0) }    
 }

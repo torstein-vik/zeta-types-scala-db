acd42306c4465190fa7b82d7f8f24c256deeb123
Torstein Vik <torsteinv64@gmail.com>
2018-05-10 23:51:55 +0200

    Added relations comment to Requirements

--- src/main/scala/query/Requirements.scala
+++ src/main/scala/query/Requirements.scala
@@ -3,4 +3,18 @@ package io.github.torsteinvik.zetatypes.db.query
 
 final class Requirements(requirements : Set[MFProperty[_]]) {
     
+    
+    /*
+    
+    Relations:
+    x -> x 
+    mf -> everything
+    belltable -> bellcell, bellrow, bellsmalltable
+    bellrow -> bellcell if correct prime
+    bellsmalltable -> bellsmalltable if dimensions smaller or equal + bellcell if exponent and prime is included 
+    
+    The latter two not implemented here
+    
+    */
+    
 }

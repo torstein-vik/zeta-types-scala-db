9ab734aab4424db27b1b4bac5b669ede2686b4a7
Torstein Vik <torsteinv64@gmail.com>
2018-05-11 00:17:54 +0200

    Added test suite for query parsing

---
+++ src/test/scala/ParserTest.scala
@@ -0,0 +1,13 @@
+package io.github.torsteinvik.zetatypes.test.db
+
+import org.scalatest.FunSuite
+
+import io.github.torsteinvik.zetatypes.db.query.parsing.Parser._
+import io.github.torsteinvik.zetatypes.db.query.Property._
+import io.github.torsteinvik.zetatypes.db.Datatypes._
+import io.github.torsteinvik.zetatypes.db._
+
+class ParserTest extends FunSuite {
+    
+    
+}

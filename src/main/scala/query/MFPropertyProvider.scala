fc973b0f8eddb496ad3167313019dc6d0335d0b2
Torstein Vik <torsteinv64@gmail.com>
2018-05-10 23:42:14 +0200

    Added ++ to combine property providers

--- src/main/scala/query/MFPropertyProvider.scala
+++ src/main/scala/query/MFPropertyProvider.scala
@@ -3,5 +3,6 @@ package io.github.torsteinvik.zetatypes.db.query
 final class MFPropertyProvider(val provider : PartialFunction[MFProperty[_], _]) {
     final def provide[T] (providee : MFProperty[T]) : T = if(provider.isDefinedAt(providee)) provider(providee).asInstanceOf[T] else throw new Exception("Property requested but not in requirements! " + providee)
     
+    final def ++ (other : MFPropertyProvider) = new MFPropertyProvider(provider orElse other.provider)
 }
 

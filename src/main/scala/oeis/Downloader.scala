d71827a58b3da4f7f0d7bdf705833e8b0b51146c
Torstein Vik <torsteinv64@gmail.com>
2018-05-11 00:24:31 +0200

    Added master file for managing and throttling download

---
+++ src/main/scala/oeis/Downloader.scala
@@ -0,0 +1,13 @@
+package io.github.torsteinvik.zetatypes.db.oeis
+
+import scala.io.Source
+
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+
+import collection.mutable.Queue
+
+import util.Try
+
+object Downloader {
+}

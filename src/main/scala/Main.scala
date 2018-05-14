fa727eb710dba8be1b15af4bd00fb44af2e7f545
Torstein Vik <torsteinv64@gmail.com>
2018-05-14 18:14:08 +0200

    Moved Main call to oeis into def

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,11 +5,16 @@ object Main extends App with REPL {
     import query.Property._
         
     // Download and store all oeis multiplicative functions in db, and use b-files for extra data
-    // oeis.Manager(mfstore, useBFile = true)
+    // downloadAndSaveOEIS()
+    
+    def downloadAndSaveOEIS() = {
+        import scala.concurrent.duration.Duration
+        oeis.Manager(mfstore, useBFile = true, timeout = Duration.Inf)
+    }
     
     // All multiplicative functions where 24 and 51 are fix-points
     // mfquery(pretty() where (mfvalue(24) ==? 24 and mfvalue(51) ==? 51)).print()
-        
+    
     // Close database
     close()
 }

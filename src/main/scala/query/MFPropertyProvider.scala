96b33bd3e48d85b49bd7a5db2488a12028b55b3d
Torstein Vik <torsteinv64@gmail.com>
2018-05-19 18:09:57 +0200

    Minor additions to MFPropertyProvider

--- src/main/scala/query/MFPropertyProvider.scala
+++ src/main/scala/query/MFPropertyProvider.scala
@@ -6,7 +6,8 @@ import io.github.torsteinvik.zetatypes.db.query.Property._
 import io.github.torsteinvik.zetatypes.db.Datatypes._
 
 final class MFPropertyProvider(val provider : PartialFunction[MFProperty[_], _]) {
-    final def provide[T] (providee : MFProperty[T]) : T = if(provider.isDefinedAt(providee)) provider(providee).asInstanceOf[T] else throw new Exception("Property requested but not in requirements! " + providee)
+    final def provide[T] (providee : MFProperty[T]) : T = if(isDefinedAt(providee)) provider(providee).asInstanceOf[T] else throw new Exception("Property requested but not in requirements! " + providee)
+    final def isDefinedAt[T] (providee : MFProperty[T]) : Boolean = provider.isDefinedAt(providee)
     
     final def ++ (other : MFPropertyProvider) = new MFPropertyProvider(provider orElse other.provider)
 }

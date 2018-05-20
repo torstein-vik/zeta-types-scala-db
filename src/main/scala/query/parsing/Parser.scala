4101cd120f4b9f67c9bd78157fda206bf88b45df
Torstein Vik <torsteinv64@gmail.com>
2018-05-20 14:14:50 +0200

    Added skeleton for toolbox-parser

--- src/main/scala/query/parsing/Parser.scala
+++ src/main/scala/query/parsing/Parser.scala
@@ -4,5 +4,24 @@ import io.github.torsteinvik.zetatypes.db._
 import io.github.torsteinvik.zetatypes.db.Datatypes._
 import io.github.torsteinvik.zetatypes.db.query._
 
+import scala.reflect.runtime._
 import scala.reflect.runtime.universe._
+import scala.tools.reflect.ToolBox
 
+import scala.util.{Try, Success, Failure}
+
+object Parser {
+    case class ParserException(msg : String) extends Exception(msg)
+    
+    private val toolBox = universe.runtimeMirror(getClass.getClassLoader).mkToolBox()
+    
+    def parse (query : String) : Query[_] = parseQuery(query).get
+    
+    private def parseQuery (query : String) : Try[Query[_]] = {
+        val tree : universe.Tree = toolBox.parse(query)
+        
+        
+        return Failure(ParserException("Not yet implemented"))
+    }
+    
+}

a8c309f30407dea94f08b6c28769a3bfdf639dfe
Torstein Vik <torsteinv64@gmail.com>
2018-05-20 14:13:30 +0200

    Updated parser-test

--- src/test/scala/ParserTest.scala
+++ src/test/scala/ParserTest.scala
@@ -4,39 +4,27 @@ import org.scalatest.FunSuite
 
 import io.github.torsteinvik.zetatypes.db.query.parsing.Parser._
 import io.github.torsteinvik.zetatypes.db.query.Property._
+import io.github.torsteinvik.zetatypes.db.query._
 import io.github.torsteinvik.zetatypes.db.Datatypes._
 import io.github.torsteinvik.zetatypes.db._
 
 class ParserTest extends FunSuite {
     
-    test ("Parsing mfproperties") {
+    test ("Parsing simple queries") {
         
-        assert(parse("mf")(mfproperty[MultiplicativeFunction]) == mf)
-        assert(parse("batchid")(mfproperty[Option[String]]) == batchid)
-        assert(parse("mflabel")(mfproperty[String]) == mflabel)
-        assert(parse("name")(mfproperty[String]) == name)
-        assert(parse("definition")(mfproperty[String]) == definition)
-        assert(parse("comments")(mfproperty[Seq[String]]) == comments)
-        assert(parse("properties")(mfproperty[Record[Boolean]]) == properties)
-        assert(parse("belltable")(mfproperty[Seq[(Prime, Seq[ComplexNumber])]]) == belltable)
+        assert(parse("mf") === (mf : Query[_]))
+        assert(parse("batchid") === (batchid : Query[_]))
+        assert(parse("mflabel") === (mflabel : Query[_]))
+        assert(parse("name") === (name : Query[_]))
+        assert(parse("definition") === (definition : Query[_]))
+        assert(parse("comments") === (comments : Query[_]))
+        assert(parse("properties") === (properties : Query[_]))
+        assert(parse("belltable") === (belltable : Query[_]))
         
-        assert(parse("bellcell(11, 34)")(mfproperty[Option[ComplexNumber]]) == bellcell(11, 34))
-        assert(parse("bellrow(7)")(mfproperty[Option[Seq[ComplexNumber]]]) == bellrow(7))
-        assert(parse("bellsmalltable(12, 12)")(mfproperty[Seq[(Prime, Seq[ComplexNumber])]]) == bellsmalltable(12, 12))
+        assert(parse("bellcell(11, 34)") === (bellcell(11, 34) : Query[_]))
+        assert(parse("bellrow(7)") === (bellrow(7) : Query[_]))
+        assert(parse("bellsmalltable(12, 12)") === (bellsmalltable(12, 12) : Query[_]))
         
-    }
-    
-    test ("Parsing literals") {
-        assert(parse("342")(literal[Int]) === 342)
-        assert(parse("-342")(literal[Int])  === -342)
-        assert(parse("342")(literal[Integer]) === Integer(342))
-        assert(parse("-342")(literal[Integer]) === Integer(-342))
-        assert(parse("12")(literal[Nat]) === Nat(12))
-        assert(parse("53")(literal[Nat]) === Nat(53))
-        assert(parse("17")(literal[Prime]) === Prime(17))
-        assert(parse("3")(literal[Prime]) === Prime(3))
-        assert(parse("""" Hey """")(literal[String]) === " Hey ")
-        assert(parse("""" Hey \" \r \\"""")(literal[String])  === " Hey \" \r \\")
     }
     
 }

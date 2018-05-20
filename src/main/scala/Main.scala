f2982617f6526d5db2d6596ee72c0b91659bac0d
Torstein Vik <torsteinv64@gmail.com>
2018-05-20 21:29:34 +0200

    Added query akin to mfget

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -16,6 +16,9 @@ object Main extends App with REPL {
     // All multiplicative functions where 24 and 51 are fix-points
     // mfquery(mfpretty() where (mfvalue(24) ==? 24 and mfvalue(51) ==? 51)).print()
     
+    // Query for a single multiplicative function
+    // mfquery(mf where (mflabel === "MF-OEIS-A000005")).print()
+    
     // All multiplicative functions which are marked as nice in OEIS
     // mfquery(mfpretty() where properties("oeis_nice")).print()
     

6a5b4120dc03260f9dbc1976bf4702b4f477f435
Torstein Vik <torsteinv64@gmail.com>
2018-05-23 01:23:39 +0200

    Added package for pretty-printing

---
+++ src/main/scala/pretty/package.scala
@@ -0,0 +1,7 @@
+package io.github.torsteinvik.zetatypes.db
+
+/** This pacakge provides prettification of properties and datatypes, both for html and latex
+ *  @author Torstein Vik
+ */
+package object pretty {
+}

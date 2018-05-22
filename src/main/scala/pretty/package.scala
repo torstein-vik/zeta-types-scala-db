339e66222b1c13e52d31d424be2a71a2bef6ca6d
Torstein Vik <torsteinv64@gmail.com>
2018-05-23 01:27:07 +0200

    Added easy-to-use defs in package for pretty, html, and latex printing

--- src/main/scala/pretty/package.scala
+++ src/main/scala/pretty/package.scala
@@ -10,4 +10,11 @@ package object pretty {
     
     type NoOptions = NoOptions.type
     
+    def pretty[T, O <: Options](t : T)(implicit pretty : Pretty[T, Plain, O], f : NoOptions => O) : String = pretty(t)(f(NoOptions))
+    def html[T, O <: Options]  (t : T)(implicit pretty : Pretty[T, HTML, O],  f : NoOptions => O) : String = pretty(t)(f(NoOptions))
+    def latex[T, O <: Options] (t : T)(implicit pretty : Pretty[T, LaTeX, O], f : NoOptions => O) : String = pretty(t)(f(NoOptions))
+    
+    def pretty[T, O <: Options](t : T, options : O)(implicit pretty : Pretty[T, Plain, O]) : String = pretty(t)(options)
+    def html[T, O <: Options]  (t : T, options : O)(implicit pretty : Pretty[T, HTML, O]) : String = pretty(t)(options)
+    def latex[T, O <: Options] (t : T, options : O)(implicit pretty : Pretty[T, LaTeX, O]) : String = pretty(t)(options)
 }

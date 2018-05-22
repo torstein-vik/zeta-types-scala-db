99e05989f8b3a8eaf941625d43bb44b08ca62e60
Torstein Vik <torsteinv64@gmail.com>
2018-05-22 23:45:07 +0200

    Made Query-defs non-final as they are now case-class

--- src/main/scala/query/Query.scala
+++ src/main/scala/query/Query.scala
@@ -3,14 +3,14 @@ package io.github.torsteinvik.zetatypes.db.query
 case class Query[T] (projection : Projection[T], filter : Predicate = TruePredicate, limit : Option[Int] = None) {
     lazy val requirements = new Requirements(projection.requires ++ filter.requires)
     
-    final def where(f : Predicate) : Query[T] = copy(
+    def where(f : Predicate) : Query[T] = copy(
         filter = if (filter == TruePredicate) f else filter and f
     )
     
-    final def take(n : Int) : Query[T] = copy(
+    def take(n : Int) : Query[T] = copy(
         limit = Some(math.min(n, limit.getOrElse(n)))
     )
     
-    final def filter(f : Predicate) : Query[T] = where(f)
-    final def limit(n : Int) : Query[T] = take(n)
+    def filter(f : Predicate) : Query[T] = where(f)
+    def limit(n : Int) : Query[T] = take(n)
 }

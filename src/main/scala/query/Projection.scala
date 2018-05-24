6bb7f65b0f0fbfe84c2e9a064a6c9b3d58718da1
Torstein Vik <torsteinv64@gmail.com>
2018-05-24 23:18:14 +0200

    Removed unused implicits in Projection

--- src/main/scala/query/Projection.scala
+++ src/main/scala/query/Projection.scala
@@ -5,7 +5,5 @@ sealed abstract class Projection[T] (val requires : Set[MFProperty[_]])
 object Projection {    
     case class PSingle[T](property : Property[T]) extends Projection[T](property.requires)
     
-    import scala.language.implicitConversions
-    implicit def apply[T](p : Property[T]) : Projection[T] = PSingle[T](p)
-    implicit def asQuery[T](p : Projection[T]) : Query[T] = Query(p)
+    def apply[T](p : Property[T]) : Projection[T] = PSingle[T](p)
 }

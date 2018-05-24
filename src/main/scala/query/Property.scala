ad6d99715a01297b2ca9aa95d5b682e45b84e887
Torstein Vik <torsteinv64@gmail.com>
2018-05-24 20:19:36 +0200

    Added implementations of apply to all compound Properties

--- src/main/scala/query/Property.scala
+++ src/main/scala/query/Property.scala
@@ -29,11 +29,28 @@ abstract sealed class CompoundProperty[T](requirements : Set[MFProperty[_]]) ext
     def apply (eval : PropertyEvaluator) : T
 } 
 
-case class ConstantProperty[T](value : T) extends CompoundProperty[T](Set())
-case class GetProperty[T](inner : Property[Option[T]]) extends CompoundProperty[T](inner.requires)
-case class ApplyProperty[T](inner : Property[Record[T]], name : String) extends CompoundProperty[Option[T]](inner.requires)
-case class TupleFirstProperty[T, S](inner : Property[(T, S)]) extends CompoundProperty[T](inner.requires)
-case class TupleSecondProperty[T, S](inner : Property[(T, S)]) extends CompoundProperty[S](inner.requires)
+case class ConstantProperty[T](value : T) extends CompoundProperty[T](Set()) {
+    def apply (eval : PropertyEvaluator) : T = value
+}
+
+case class GetProperty[T](inner : Property[Option[T]]) extends CompoundProperty[T](inner.requires) {
+    def apply (eval : PropertyEvaluator) : T = eval(inner) match {
+        case Some(y) => y
+        case None => throw new Exception("Value assumed to exist in query did not exist! Property: " + inner)
+    }
+}
+
+case class ApplyProperty[T](inner : Property[Record[T]], name : String) extends CompoundProperty[Option[T]](inner.requires) {
+    def apply (eval : PropertyEvaluator) : Option[T] = eval(inner).entries.find(_._1 == name).map(_._2)
+}
+
+case class TupleFirstProperty[T, S](inner : Property[(T, S)]) extends CompoundProperty[T](inner.requires) {
+    def apply (eval : PropertyEvaluator) : T = eval(inner)._1
+}
+
+case class TupleSecondProperty[T, S](inner : Property[(T, S)]) extends CompoundProperty[S](inner.requires) {
+    def apply (eval : PropertyEvaluator) : S = eval(inner)._2
+}
 
 case class PropertyLambda[T](output : Predicate)
 case class LambdaInputProperty[T]() extends Property[T] {def requires = Set()}
@@ -56,9 +73,24 @@ object Property {
     case class bellrow(p : Prime) extends MFProperty[Seq[ComplexNumber]]
     case class bellsmalltable(ps : Int = 10, es : Int = 15) extends MFProperty[Seq[(Prime, Seq[ComplexNumber])]]
     
-    case class mfpretty(ps : Int = 10, es : Int = 15) extends CompoundProperty[String](Set(bellsmalltable(ps, es), mflabel, name, definition))
+    case class mfpretty(ps : Int = 10, es : Int = 15) extends CompoundProperty[String](Set(bellsmalltable(ps, es), mflabel, name, definition)) {
+        def apply (eval : PropertyEvaluator) : String = f"Label: ${eval(mflabel)} \t Name: ${eval(name)} \nDescription: ${eval(definition)} \n\nBell Table: \n" + (
+            for {(Prime(prime), vals) <- eval(bellsmalltable(ps, es))} 
+                yield f"p=${prime}: \t " + vals.map(_.pretty).mkString(", \t")).mkString("\n")
+    }
     case class mfvalue(n : Nat) extends CompoundProperty[Option[ComplexNumber]](Factor(n).toSet.map(bellcell.tupled)) {
         val factors : Set[bellcell] = requires.asInstanceOf[Set[bellcell]]
+        
+        def apply (eval : PropertyEvaluator) : Option[ComplexNumber] = n match {
+            case _ if n == 0 => Some(new Nat(0)) 
+            case _ if n == 1 => Some(new Nat(1))
+            case _ => {
+                val parts = factors.map(eval(_))
+                if (parts.exists(_.isEmpty)) None else Some(parts.map(_.get).foldLeft[ComplexNumber](Integer(1))({
+                    case (acc, next) => CartesianComplex(Floating(acc.re * next.re - acc.im * next.im), Floating(acc.re * next.im + acc.im * next.re))
+                }))
+            }
+        }
     }
     
     implicit final class StringProperty(prop : Property[String]) {

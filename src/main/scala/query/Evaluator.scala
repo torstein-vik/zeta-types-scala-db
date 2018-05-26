1d533c541da0a0c62ea8a8bdd51f354b38300aff
Torstein Vik <torsteinv64@gmail.com>
2018-05-26 18:51:53 +0200

    Cleanup in Evaluator

--- src/main/scala/query/Evaluator.scala
+++ src/main/scala/query/Evaluator.scala
@@ -3,23 +3,23 @@ package io.github.torsteinvik.zetatypes.db.query
 trait Evaluator {
     def apply[T] (p : Property[T]) : T
     def apply (p : Predicate) : Boolean
+    def apply[T] (f : PropertyLambda[T], input : T) : Boolean
 }
 
 object StandardEvaluator {
-    
     private sealed abstract class EvalContext
     private case object NoContext extends EvalContext
     private case class LambdaContext[T](t : T) extends EvalContext
-    
 }
 
 final class StandardEvaluator (mf : MFPropertyProvider) extends Evaluator {
     import StandardEvaluator._
     
-    def apply[T] (p : Property[T]) : T = evalProperty(p, mf)(NoContext)
-    def apply (p : Predicate) : Boolean = evalPredicate(p, mf)(NoContext)
+    def apply[T] (p : Property[T]) : T = evalProperty(p)(NoContext)
+    def apply (p : Predicate) : Boolean = evalPredicate(p)(NoContext)
+    def apply[T] (f : PropertyLambda[T], input : T) : Boolean = evalPredicate(f.output)(LambdaContext[T](input))
     
-    private def evalProperty[T](p : Property[T], mf : MFPropertyProvider)(implicit ctx : EvalContext) : T = p match {
+    private def evalProperty[T](p : Property[T])(implicit ctx : EvalContext) : T = p match {
         case p : MFProperty[T] => mf(p)
         case p : CompoundProperty[T] => p(this)
         
@@ -29,26 +29,23 @@ final class StandardEvaluator (mf : MFPropertyProvider) extends Evaluator {
         }
     }
     
-    private def evalPredicate(p : Predicate, mf : MFPropertyProvider)(implicit ctx : EvalContext) : Boolean = p match {
+    private def evalPredicate(p : Predicate)(implicit ctx : EvalContext) : Boolean = p match {
         case TruePredicate => true
         
-        case EqualityPredicate(prop1, prop2) => evalProperty(prop1, mf) == evalProperty(prop2, mf)
-        case StringContainsPredicate(superstr, substr) => evalProperty(superstr, mf) contains evalProperty(substr, mf)
-        case RegexPredicate(str, regex) => evalProperty(str, mf) match {case regex() => true case _ => false}
+        case EqualityPredicate(prop1, prop2) => evalProperty(prop1) == evalProperty(prop2)
+        case StringContainsPredicate(superstr, substr) => evalProperty(superstr) contains evalProperty(substr)
+        case RegexPredicate(str, regex) => evalProperty(str) match {case regex() => true case _ => false}
         
-        case SeqContainsPredicate(seq, element) => evalProperty(seq, mf) contains evalProperty(element, mf)
-        case SeqHasPredicate(seq, pred) => evalProperty(seq, mf).exists(evalPropertyLambda(pred, _, mf))
-        case SeqAllPredicate(seq, pred) => evalProperty(seq, mf).forall(evalPropertyLambda(pred, _, mf))
+        case SeqContainsPredicate(seq, element) => evalProperty(seq) contains evalProperty(element)
+        case SeqHasPredicate(seq, pred) => evalProperty(seq).exists(apply(pred, _))
+        case SeqAllPredicate(seq, pred) => evalProperty(seq).forall(apply(pred, _))
         
-        case ExistsPredicate(opt) => !evalProperty(opt, mf).isEmpty
+        case ExistsPredicate(opt) => !evalProperty(opt).isEmpty
         
-        case AndPredicate(pred1, pred2) => evalPredicate(pred1, mf) && evalPredicate(pred2, mf)
-        case OrPredicate(pred1, pred2) => evalPredicate(pred1, mf) || evalPredicate(pred2, mf)
-        case NotPredicate(pred) => !evalPredicate(pred, mf)
+        case AndPredicate(pred1, pred2) => evalPredicate(pred1) && evalPredicate(pred2)
+        case OrPredicate(pred1, pred2) => evalPredicate(pred1) || evalPredicate(pred2)
+        case NotPredicate(pred) => !evalPredicate(pred)
         
-        case BooleanPredicate(prop) => evalProperty(prop, mf)
-    }
-    
-    private def evalPropertyLambda[T](lambda : PropertyLambda[T], v : T, mf : MFPropertyProvider) : Boolean = evalPredicate(lambda.output, mf)(LambdaContext[T](v))
-    
+        case BooleanPredicate(prop) => evalProperty(prop)
+    }    
 }

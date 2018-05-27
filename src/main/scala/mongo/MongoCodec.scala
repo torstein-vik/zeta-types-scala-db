9b6ab84d9ae80706dcc23ef25691a9df86c19415
Torstein Vik <torsteinv64@gmail.com>
2018-05-27 14:55:40 +0200

    Added encode and decode Bson to MongoCodec

--- src/main/scala/mongo/MongoCodec.scala
+++ src/main/scala/mongo/MongoCodec.scala
@@ -3,10 +3,13 @@ package io.github.torsteinvik.zetatypes.db.mongo
 import org.json4s._
 import org.json4s.native.JsonMethods._
 
-import org.mongodb.scala._
+import org.mongodb.scala.bson._
 
 // TODO: Implement a better codec.
 object MongoCodec {
+    def encodeBson (x : JValue) : BsonValue = encode(JObject(List(JField("value", x)))).apply[BsonValue]("value")
+    def decodeBson (x : BsonValue) : JValue = decode(Document("value" -> x)) \ "value"
+    
     def encode (x : JValue) : Document = Document(compact(render(x)))
     def decode (x : Document) : JValue = parse(x.toJson)
 }

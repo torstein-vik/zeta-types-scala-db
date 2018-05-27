3360fda37f0a6f9df37839f655f8e86ebd6de103
Torstein Vik <torsteinv64@gmail.com>
2018-05-27 16:06:04 +0200

    Reorganized imports in MongoCodec

--- src/main/scala/mongo/MongoCodec.scala
+++ src/main/scala/mongo/MongoCodec.scala
@@ -1,9 +1,11 @@
 package io.github.torsteinvik.zetatypes.db.mongo
 
 import org.json4s._
-import org.json4s.native.JsonMethods._
 
 import org.mongodb.scala.bson._
+import org.bson.BsonType._
+
+import scala.collection.JavaConverters._
 
 // TODO: Implement a better codec.
 object MongoCodec {

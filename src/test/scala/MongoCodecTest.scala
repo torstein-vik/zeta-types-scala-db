b82c8aad825b4f0bc0a9890f1dcfec1aa92d2288
Torstein Vik <torsteinv64@gmail.com>
2018-05-27 14:56:36 +0200

    Added test for encoding to bson

--- src/test/scala/MongoCodecTest.scala
+++ src/test/scala/MongoCodecTest.scala
@@ -10,6 +10,8 @@ import org.json4s.native.JsonMethods._
 import org.mongodb.scala._
 import org.mongodb.scala.bson.{Document => _, _}
 
+import scala.collection.JavaConverters._
+
 class MongoCodecTest extends FunSuite {
     test ("MongoCodec for documents") {
         val json = """
@@ -30,5 +32,16 @@ class MongoCodecTest extends FunSuite {
         assert(MongoCodec.decode(doc) === json4s)
         
         
+    }
+    
+    test ("MongoCodec encode to Bson") {
+        
+        assert(MongoCodec.encodeBson(JString("hey")).asString.getValue() === "hey")
+        assert(MongoCodec.encodeBson(JInt(12)).asNumber.intValue() === 12)
+        assert(MongoCodec.encodeBson(JDouble(12302.4553)).asNumber.doubleValue() === 12302.4553)
+        assert(MongoCodec.encodeBson(JNull).isNull())
+        assert(MongoCodec.encodeBson(JBool(true)).asBoolean.getValue() === true)
+        assert(MongoCodec.encodeBson(JArray(List(JInt(12), JInt(24), JInt(34)))).asArray.getValues().asScala.map(_.asNumber.doubleValue()) === Seq(12, 24, 34))
+                
     }
 }
